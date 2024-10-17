22280f64930dd0853a0c5ddb801f2931669ddb27
zulmkodr <>
2024-10-17 12:24:06 +1100

    added unittests

--- tests/unittests.py
+++ tests/unittests.py
@@ -1,10 +1,30 @@
 import unittest
-from mkdocs_asyncapi_tag.mkdocs_asyncapi_plugin import AsyncAPIPlugin
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.files import Files
+from asyncapi_plugin import AsyncAPIPlugin
 
-class TestSimple(unittest.TestCase):
-    def test_module_path(self):
-        path = AsyncAPIPlugin.getModulePath()
-        print(path)
-        self.assertEqual(testModule.name, "test-module")
-    if __name__ == '__main__':
-        unittest.main()
+class TestAsyncAPIPlugin(unittest.TestCase):
+
+    def setUp(self):
+        self.plugin = AsyncAPIPlugin()
+        self.config = MkDocsConfig()
+        self.config['docs_dir'] = 'docs'
+        self.config['site_dir'] = 'site'
+        self.config['use_directory_urls'] = False
+
+    def test_on_files(self):
+        files = Files([])
+        self.plugin.config['asyncapi_file'] = 'schema.json'
+        result = self.plugin.on_files(files, self.config)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].path, 'schema.json')
+
+    def test_on_page_content(self):
+        html = '<asyncapi-tag src="schema.json"></asyncapi-tag>'
+        page = None
+        files = None
+        result = self.plugin.on_page_content(html, page, self.config, files)
+        self.assertIn('schema.json', result)
+
+if __name__ == '__main__':
+    unittest.main()
